270e092448a9119cde576fc854327c1ab8a3f018
caeleb <caelebadamsbalanesi@gmail.com>
2025-10-02 19:08:16 -0400

    Began creating the transition table for variable declaration and variable assignment. Created file input code section.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+enum States {
+    START,
+    ASSIGN_LHS,
+    ASSIGN_VALUE,
+    VAR_DEC,
+    VAR_NAME_DEC,
+    VAR_INIT,
+}
+
+enum Events {
+    NUM,
+    DEC,
+    IDENTIFIER,
+    VALUE,
+    END,
+}
+
+void main() {
+
+    // transition_table[current_state][event] = next_state
+    States[][] transition_table = new States[States.values().length][Events.values().length];
+
+    // START
+    transition_table[States.START.ordinal()][Events.NUM.ordinal()] = States.VAR_DEC; // START -num-> VAR_DEC
+    transition_table[States.START.ordinal()][Events.DEC.ordinal()] = States.VAR_DEC; // START -dec-> VAR_DEC
+    transition_table[States.START.ordinal()][Events.IDENTIFIER.ordinal()] = States.ASSIGN_LHS; // START -identifier-> ASSIGN_LHS
+
+    // VAR_DEC
+    transition_table[States.VAR_DEC.ordinal()][Events.IDENTIFIER.ordinal()] = States.VAR_NAME_DEC; // VAR_DEC -identifier-> VAR_NAME_DEC
+
+    // VAR_NAME_DEC
+    transition_table[States.VAR_NAME_DEC.ordinal()][Events.VALUE.ordinal()] = States.VAR_INIT; // VAR_NAME_DEC -value-> VAR_INIT
+    transition_table[States.VAR_NAME_DEC.ordinal()][Events.END.ordinal()] = States.START; // VAR_NAME_DEC -end-> START
+
+    // VAR_INIT
+    transition_table[States.VAR_INIT.ordinal()][Events.END.ordinal()] = States.START; // VAR_INIT -end-> START
+
+    // ASSIGN_LHS
+    transition_table[States.ASSIGN_LHS.ordinal()][Events.VALUE.ordinal()] = States.ASSIGN_VALUE; // ASSIGN_LHS -value-> ASSIGN_VALUE
+
+    // ASSIGN_VALUE
+    transition_table[States.ASSIGN_VALUE.ordinal()][Events.END.ordinal()] = States.START; // ASSIGN_VALUE -end-> START
+
+    try {
+        File myFile = new File("main.cj");
+        Scanner myReader = new Scanner(myFile);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+
+        }
+        myReader.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred: File not found.");
+        e.printStackTrace();
     }
+
 }
